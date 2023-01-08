7c8c85dcb70f3ea2241cee22ecd82c8cdc8f41e3
franceskora <francesanyakora@gmail.com>
2023-01-08 04:02:11 -0800

    my commit

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,5 +1,5 @@
 def magic_calculation(a, b):
-    from magic_calculation_102 import add,sub
+    from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
