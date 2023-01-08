21bf60c475acde9e1a7a3f590434dd350256d39d
franceskora <francesanyakora@gmail.com>
2023-01-08 08:20:46 -0800

    my commit

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-
 def print_matrix_integer(matrix=[[]]):
-    if matrix != [[]]:
-        for row in matrix:
-            for elem in row:
-                print("{:d}".format(elem), end=" "
-                        if elem != row[-1] else '\n')
+    for row in matrix:
+        for i in range(len(row)):
+            if i < (len(row) - 1):
+                print('{:d}'.format(row[i]), end=' ')
             else:
-                print()
+                print('{:d}'.format(row[i]), end=' '
+            print()
