b09603e70f3fb26677e361319fb752b658fac455
franceskora <francesanyakora@gmail.com>
2023-01-11 13:46:29 -0800

    my commit

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dictionary = a_dictionary.copy()
-    for key, value in list(new_dictionary.items()):
-        new_dictionary[key] = value * 2
-        return new_dictionary
+    new_dict = a_dictionary.copy()
+
+    for item in new_dict:
+        new_dict[item] *= 2
+
+        return new_dict
