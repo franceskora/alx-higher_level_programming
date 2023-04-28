b30802916b9fc90fd16ba343eba1fb6547b1eea3
franceskora <francesanyakora@gmail.com>
2023-04-28 15:10:38 -0700

    my commit

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,9 @@
-i#!/usr/bin/python3
+#!/usr/bin/python3
 """
 fetches https:://alx-intranet.hbtn.io/status
 """
 if __name__ == "__main__":
-    import urlib.request as request
+    import urllib.request as request
     with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         html = response.read()
         print('Body response:')
